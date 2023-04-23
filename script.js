c3dcf28e6fb493f474c403250d47853824793ba1
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-22 19:19:19 -0500

    made append method add node at end of list instead of replacing last node. Also made a size counter to linked list

--- script.js
+++ script.js
@@ -9,16 +9,25 @@ function nodeFactory(value, nextNode) {
 function linkedListFactory() {
   return {
     head: null,
+    size: 0,
     append: function (value) {
       let node = nodeFactory(value);
       if (this.head === null) {
         this.head = node;
+        this.size += 1;
       } else {
+        let current = this.head;
+        while (current.nextNode) {
+          current = current.nextNode;
+        }
+        current.nextNode = node;
         this.tail = node;
+        this.size += 1;
       }
     },
     prepend: function (value) {
       this.head = nodeFactory(value, this.head);
+      this.size += 1;
     },
   };
 }
